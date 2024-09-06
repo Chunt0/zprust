dedcdb55325dc83f011ec08634de73ee762fa925
Chunt0 <chunt.mail@gmail.com>
2024-09-06 15:57:34 -0700

    2024-09-06 15:57:34

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -26,14 +26,18 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 # Check if a custom host hasbeen set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
-#Launch postgres using Docker
-docker run \
-	-e POSTGRES_USER=${DB_USER} \
-	-e POSTGRES_PASSWORD=${DB_PASSWORD} \
-	-e POSTGRES_DB=${DB_NAME} \
-	-p "${DB_PORT}":5432 \
-	-d postgres \
-	postgres -N 1000
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+	#Launch postgres using Docker
+	docker run \
+		-e POSTGRES_USER=${DB_USER} \
+		-e POSTGRES_PASSWORD=${DB_PASSWORD} \
+		-e POSTGRES_DB=${DB_NAME} \
+		-p "${DB_PORT}":5432 \
+		-d postgres \
+		postgres -N 1000
+fi
+
 
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
@@ -47,3 +51,6 @@ done
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
